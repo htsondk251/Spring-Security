4cf065ca888664a2cdbe842745cc80ad043682f8
htsondk251 <thanh_son_hoang_70@yahoo.com>
2021-02-20 17:16:10 +0700

    default configuration

---
+++ ch2/ch2-ex1/src/main/java/com/example/ch2ex1/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.ch2ex1.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping(value="/hello")
+    public String Hello() {
+        return "Hello!";
+    }
+    
+}
