1e35dd4f28ad526eb4ae21a4d82b2eb297dd32c8
htsondk251 <thanh_son_hoang_70@yahoo.com>
2021-02-24 15:38:12 +0700

    separate responsibilities
    using UserManagementConfig for user management
    using WebAuthorizationConfig for authorization

---
+++ ch2/ch2-ex6/src/main/java/com/example/ch2ex6/Ch2ex6Application.java
@@ -0,0 +1,13 @@
+package com.example.ch2ex6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch2ex6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch2ex6Application.class, args);
+	}
+
+}
