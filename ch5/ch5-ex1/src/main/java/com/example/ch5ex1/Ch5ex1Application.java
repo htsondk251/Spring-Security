af4cf0774debdc0b9891bfaf2d54a4383a2f658c
htsondk251 <thanh_son_hoang_70@yahoo.com>
2021-02-28 11:12:46 +0700

    implement CustomAuthenticationProvider

---
+++ ch5/ch5-ex1/src/main/java/com/example/ch5ex1/Ch5ex1Application.java
@@ -0,0 +1,13 @@
+package com.example.ch5ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch5ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch5ex1Application.class, args);
+	}
+
+}
