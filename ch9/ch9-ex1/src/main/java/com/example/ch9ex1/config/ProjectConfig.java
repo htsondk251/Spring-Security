552db06a7df1672278f6e5c601e1013a883d834c
htsondk251 <thanh_son_hoang_70@yahoo.com>
2021-03-04 10:06:39 +0700

    add filter(s) before & after other filter

---
+++ ch9/ch9-ex1/src/main/java/com/example/ch9ex1/config/ProjectConfig.java
@@ -0,0 +1,20 @@
+package com.example.ch9ex1.config;
+
+import com.example.ch9ex1.filter.AuthenticationLoggingFilter;
+import com.example.ch9ex1.filter.RequestValidationFilter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class)
+            .addFilterAfter(new AuthenticationLoggingFilter(), BasicAuthenticationFilter.class)
+            .authorizeRequests()
+                .anyRequest().permitAll();
+    }
+}
