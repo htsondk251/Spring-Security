c1c9fbadc90469b455f56a48576f5593022269c4
htsondk251 <thanh_son_hoang_70@yahoo.com>
2021-03-04 12:06:12 +0700

    add filter at the same position with other filter - static key

---
+++ ch9/ch9-ex2/src/main/java/com/example/ch9ex2/filter/StaticKeyAuthenticationFilter.java
@@ -0,0 +1,35 @@
+package com.example.ch9ex2.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StaticKeyAuthenticationFilter implements Filter {
+
+    @Value("${authorization.key}")
+    private String authorizationKey;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        
+        var httpRequest = (HttpServletRequest) request;
+        var httpResponse = (HttpServletResponse) response;
+
+        String authentication = httpRequest.getHeader("Authorization");
+        if (authorizationKey.equals(authentication)) {
+            filterChain.doFilter(request, response);
+        } else {
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+}
