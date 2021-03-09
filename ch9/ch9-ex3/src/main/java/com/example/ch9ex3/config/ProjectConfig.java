11830519de29da1347dd0f3141f8444227887856
htsondk251 <thanh_son_hoang_70@yahoo.com>
2021-03-09 07:56:36 +0700

    implement OncePerRequestFilter
    description:
    app check if the request's header "Request-Id" is set or not and log to the console
    
    guide:
    run command
    curl -v -H "Request-Id: 12345" http://localhost:8080/hello
    to check

---
+++ ch9/ch9-ex3/src/main/java/com/example/ch9ex3/config/ProjectConfig.java
@@ -0,0 +1,20 @@
+package com.example.ch9ex3.config;
+
+import com.example.ch9ex3.filter.AuthenticationLoggingFilter;
+import com.example.ch9ex3.filter.RequestValidationFilter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class)
+            .addFilterAfter(new AuthenticationLoggingFilter(), BasicAuthenticationFilter.class)
+            .authorizeRequests().anyRequest().permitAll();
+    }
+}
