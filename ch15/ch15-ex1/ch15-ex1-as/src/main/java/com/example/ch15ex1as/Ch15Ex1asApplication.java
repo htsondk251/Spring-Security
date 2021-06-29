6371d016bb59af081c043f49c119fbe046f1916d
htsondk251 <thanh_son_hoang_70@yahoo.com>
2021-06-29 23:32:20 +0700

    implement jwt symmetric key

---
+++ ch15/ch15-ex1/ch15-ex1-as/src/main/java/com/example/ch15ex1as/Ch15Ex1asApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch15ex1as;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch15Ex1asApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch15Ex1asApplication.class, args);
+	}
+
+}
---
+++ ch15/ch15-ex1/ch15-ex1-as/src/test/java/com/example/ch15ex1/Ch15Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch15ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch15Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
