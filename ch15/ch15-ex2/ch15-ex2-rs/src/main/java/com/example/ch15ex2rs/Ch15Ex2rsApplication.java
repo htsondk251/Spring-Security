eb75f52a8efacfd4e583f041c08262bb95abe4b5
htsondk251 <thanh_son_hoang_70@yahoo.com>
2021-06-30 00:02:33 +0700

    implement jwt asymmetric key pair

---
+++ ch15/ch15-ex2/ch15-ex2-rs/src/main/java/com/example/ch15ex2rs/Ch15Ex2rsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch15ex2rs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch15Ex2rsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch15Ex2rsApplication.class, args);
+	}
+
+}
